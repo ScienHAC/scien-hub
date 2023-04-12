7af28302b1f28e25a5d0c13dd72c2b6105d196b7
Piyush kumar <90610178+ScienHAC@users.noreply.github.com>
2023-04-12 21:41:05 +0530

    Update save-comment.php

--- save-comment.php
+++ save-comment.php
@@ -1,5 +1,6 @@
 <?php
 header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Headers: *");
 // read the existing comments from the JSON file
 $comments = json_decode(file_get_contents("https://scienhac.github.io/scien-hub/file.json"), true);
 
