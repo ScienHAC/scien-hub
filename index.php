e13fd7b3b47bd6e0dd0c1adf82e750db6f4bea1e
Piyush kumar <90610178+ScienHAC@users.noreply.github.com>
2023-04-12 13:27:33 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,1 +1,16 @@
+<?php
+// Define the message content
+$title = "Welcome to my website!";
+$text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris eget enim eros. Praesent ultricies purus quis justo sollicitudin vestibulum. Donec congue vel risus sed ullamcorper. Nullam euismod vel nisl a viverra. Nam eu pharetra metus. Nam euismod est eget ex varius, in tempor velit porttitor. Vivamus vel ultricies elit. Donec eget elit tellus. Sed mollis sapien ac tellus pretium, vel luctus odio pharetra. Aliquam erat volutpat. Nulla vel blandit justo, a semper ipsum. Nam scelerisque risus at gravida molestie. Nullam feugiat magna quis ex rutrum, ac blandit mauris venenatis. Sed blandit lectus eget ex dignissim, a lacinia quam sagittis. Duis tincidunt consectetur urna sed bibendum. Nam id leo id ipsum tincidunt faucibus. Donec in risus vel sem eleifend gravida vitae et lorem.";
 
+// Define the HTML code for the message
+$html = <<<HTML
+  <div class="alert alert-info" role="alert">
+    <h4 class="alert-heading">$title</h4>
+    <hr>
+    <p>$text</p>
+  </div>
+HTML;
+
+// Output the HTML code
+echo $html;
