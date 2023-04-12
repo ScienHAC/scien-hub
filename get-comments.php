83d51b1567a97fc6aa7e052c9a8027924e96900a
Piyush kumar <90610178+ScienHAC@users.noreply.github.com>
2023-04-12 20:29:43 +0530

    Create get-comments.php

---
+++ get-comments.php
@@ -0,0 +1,6 @@
+// read the comments from the JSON file
+$comments = json_decode(file_get_contents("https://scienhac.github.io/scien-hub/file.json"), true);
+
+// return the comments as a JSON response
+header("Content-Type: application/json");
+echo json_encode($comments);
