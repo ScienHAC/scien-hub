431f7572aede93133c215390104eef9ef7eba788
Piyush kumar <90610178+ScienHAC@users.noreply.github.com>
2023-04-12 13:05:41 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,15 @@
-alert('hello');
+ var typed = new Typed('.type_ty', {  
+    strings: [  
+        "Welcome to ScienHAC",  
+        "Learn coding",  
+        "machine learning",  
+        "web development",  
+        "ethical hacking",  
+        "and other awesome useful content :) "
+        ],  
+    typeSpeed: 15,  
+    backSpeed: 10,  
+    loop: true,  
+    showCursor: false,  
+    backDelay: 1500,  
+   });  
