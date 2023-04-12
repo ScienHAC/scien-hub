b693fbb17d838396808be8d67aef3f7c2a491f1c
Piyush kumar <90610178+ScienHAC@users.noreply.github.com>
2023-04-12 13:18:29 +0530

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php
-ECHO "Hello World!<br>";
-echo "Hello World!<br>";
-EcHo "Hello World!<br>";
+// Define the alert message and type
+$message = "This is an alert message.";
+$type = "success"; // can be "success", "info", "warning", or "danger"
+
+// Define the HTML code for the alert
+$html = <<<HTML
+  <div class="alert alert-$type" role="alert">
+    $message
+  </div>
+HTML;
+
+// Output the HTML code
+echo $html;
 ?>
