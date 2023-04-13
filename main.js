05d6d609778660c3d98a138bf049742ac07a3e95
Piyush kumar <90610178+ScienHAC@users.noreply.github.com>
2023-04-13 15:44:42 +0530

    Update main.js

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ let type_script = document.createElement('script');
 type_script.setAttribute('type', 'text/javascript');
 type_script.setAttribute('src', 'https://s3.amazonaws.com/myadvobuck/static/libs/typed.min.js');
 document.head.appendChild(type_script);
+console.log(type_script.innerHTML);
 var typed = new Typed('.type_ty', {  
     strings: [  
         "Welcome to ScienHAC",  
