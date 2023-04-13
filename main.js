8687bdac48c28c0cb4bb195ffca8f539de9e5e6f
Piyush kumar <90610178+ScienHAC@users.noreply.github.com>
2023-04-13 15:49:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,8 +1,9 @@
 let type_script = document.createElement('script');
 type_script.setAttribute('type', 'text/javascript');
 type_script.setAttribute('id', 'typed');
-type_script.setAttribute('src', 'https://s3.amazonaws.com/myadvobuck/static/libs/typed.min.js');
+type_script.src = 'https://s3.amazonaws.com/myadvobuck/static/libs/typed.min.js';
 document.head.appendChild(type_script);
+document.body.appendChild(type_script);
 console.log(document.querySelector('#typed').innerHTML);
 var typed = new Typed('.type_ty', {  
     strings: [  
